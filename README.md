eb723736f6b6a704d5b972aef174f117ea6c10e5
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2023-10-07 14:42:27 -0400

    finished HW

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # MakeChangeProject
-test
+
+## Programs Used
+		*java 8
+		*eclipse IDE
+		*GitHub repository /Git
+		*Terminal
+
+
+
+##  What I Learned
+		This assignment helped me better understand concepts such as IF, Else statements. The proper way to initialize my variables and also the importance of a semi-colon after every statement.
+		What the proper usage of math expressions and how to implement them in my code. Understanding methods and how scope can be an issue if not in between the proper set of {} braces. I also learned to write my programs 
+		in a way that aids someone who does not know how to use it, and to be keen on users with malicious intent.
+
+
+## The product:
+	Hello ladies and gentlemen this is the latest and greatest software for your company's cash register. Don't believe me? Let me show you...
+	Introducing Register 1.0.
+	This software is extremely intelligent built with the best programming language in the world. JAVA (8)
+	Register 1.0, or Regi as we call it. Allows users to accurately know how much change to dispense. That includes the money owed to the customer, and shows the exact denomination of bills/coins needed to 
+	be returned to the customer. With our patent pending UI (User Interface) it reduces the amount of erroneous transactions by and astounding 30%.
+	You simply tell Regi the price of the item and how much money was received from the customer. After that, Regi calculates the  return and thanks to beautiful magic that is called Math it prints outs an easy to read message. 	Regi leaves enough space in between lines to ensure that the user reads it  without straining their eyes. So for you business folk, that translates to less employees complaining of eye fatigue. 
+	There has never been a better time to upgrade your cash register. call (111)-111-1111 and say Osvaldo sent you for an additional 10% off Purchase price. Thank you!
+	 
+	 
--- src/MakeChange/Register.java
+++ src/MakeChange/Register.java
@@ -1,7 +1,82 @@
 package MakeChange;
+
+import java.util.Scanner;
+
 public class Register {
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Price of item: ");
+		double price = sc.nextDouble();
+
+		System.out.println("Cash tendered: ");
+		double cashRecieved = sc.nextDouble();
+
+		double changeDue = cashRecieved - price;
+		changeDue += 0.004;
+
+		int twenties = 0;
+		int tens = 0;
+		int fives = 0;
+		int ones = 0;
+		int quaters = 0;
+		int dimes = 0;
+		int nickles = 0;
+		int penny = 0;
 		
+		sc.close();
+
+		if (cashRecieved == price) {
+			System.out.println("No change required... Tell customer to have a great day! ");
+		}
+		else if (cashRecieved < price) {
+			System.out.println("Insufficient Funds.... Ask Customer for remainder of funds.");
+		} else {
+			System.out.printf("%.2f  : Cash to be returned |\n  ", changeDue );
+			if (changeDue / 20 >= 1) {
+				twenties = (int) changeDue / 20;
+				changeDue = changeDue % 20;
+			}
+			if (changeDue / 10 >= 1) {
+				tens = (int) changeDue / 10;
+				changeDue = changeDue % 10;
+				System.out.println("\n Ten Dollar Bills: " + tens);
+			}
+			if (changeDue / 5 >= 1) {
+				fives = (int) changeDue / 5;
+				changeDue = changeDue % 5;
+				System.out.println("\n Five Dollar Bills: " + fives);
+
+			}
+			if (changeDue / 1 >= 1) {
+				ones = (int) changeDue / 1;
+				changeDue = changeDue % 1;
+				System.out.println("\n One Dollar Bills: " + ones);
+
+			}
+			if (changeDue / 0.25 >= 1) {
+				quaters = (int) (changeDue / 0.25);
+				changeDue = changeDue % 0.25;
+				System.out.println("\n Quaters: " + quaters);
+			}
+			if (changeDue / 0.10 >= 1) {
+				dimes = (int) (changeDue / 0.10);
+				changeDue = changeDue % 0.10;
+				System.out.println("\n Dimes: " + dimes);
+			}
+			if (changeDue / 0.05 >= 1) {
+				nickles = (int) (changeDue / 0.05);
+				changeDue = changeDue % 0.05;
+				System.out.println("\n Nickles: " + nickles);
+
+			}
+			if (changeDue / 0.01 >= 1) {
+				penny = (int) (changeDue / 0.01);
+				changeDue = changeDue % 0.01;
+				System.out.println("\n Pennies: " + penny);
+				}
+		}
+
 	}
 
 }
